fc54024b3bacf1a93ea7a5777db493004f60bee9
purple-wind <clever_programmer@163.com>
2020-12-23 10:39:00 +0800

    Update main.cpp

--- move/main.cpp
+++ move/main.cpp
@@ -3,10 +3,11 @@
 #include<string.h>
 #include<type_traits>
 
-// 	牛叉的std::move()，标准库使用模板类型推断和引用折叠这一特性的经典例子
+// 	牛叉的std::move()，标准库使用模板类型推断和引用折叠这一特性的经典例子，由于模板类型推断和引用折叠的特性，
+// my_move既可以接受左值实参，也可以接受由值实参，都能表现出正确的行为。只是接受右值实参的时候my_move什么都不做而已。
 template<typename T> typename std::remove_reference<T>::type&& my_move ( T&& t )
 {
-    return static_cast<typename std::remove_reference<T>::type&&> ( t );//将一个右值转换成一个右值引用，此时左值被截断。
+    return static_cast<typename std::remove_reference<T>::type&&> ( t );//将一个左值转换成一个右值引用，此时左值被截断。
 }
 
 int main()
