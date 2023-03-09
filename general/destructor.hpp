510f8dfbd04037a135b66528d0ec1e265186be96
purple-wind <clever_programmer@163.com>
2023-03-09 15:55:11 +0800

    演示类析构时成员析构的时机

--- general/destructor.hpp
+++ general/destructor.hpp
@@ -17,10 +17,18 @@ class DelArray{
             {
                 a[i].a = i;
             }
+            b.a = 100;
             std::cout<<"after constructor"<<std::endl;
+        }
+        ~DelArray()
+        {
+            //析构的时候先调用析构函数中的语句，该函数节航速后再析构成员变量。
+            //此函数中没有手动释放a b, a、b是该函数结束后自动被析构的
+            std::cout<<__FUNCTION__<<std::endl;
         }
     private:
         A a[5];
+        A b;
 };
 
 
