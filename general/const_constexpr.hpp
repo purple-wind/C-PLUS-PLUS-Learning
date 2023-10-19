b7ab2ebc3744372da4b1db5e3dd3932acde0e409
purple-wind <clever_programmer@163.com>
2023-10-19 19:12:03 +0800

    static const constexpr等修饰对类内初始化的影响

--- general/const_constexpr.hpp
+++ general/const_constexpr.hpp
@@ -254,12 +254,48 @@ class LiteralConstClass7
 };
 
 #include<vector>
-//class CC
-//{
-//    public:
-//      static constexpr  char* arr[4] = {"123", "456"};
-////      static const std::vector<int> arr1 = {1,2,3};
-//      static constexpr double d0 = 0;
-//      static constexpr LiteralConstClass1 d1 = {'1', 2, 3.3};
+class LiteralConstClass11
+{
+    public:
+        char a;
+        int b;
+        double c;
+        void fun0123(){}
+};
 
-//};
+//类的constexpr静态成员变量
+//1.constexpr的静态成员变量必须在类内初始化,不能在类内申明类外定义
+//2.类内初始化只能是列表初始化方式，不能是其他方式，故类型要遵守列表初始化的限制
+//3.constexpr的静态成员变量类型支持整型、 浮点型、一维数组、字面值类型，支持多维数组但是在使用时只能取最底层维度的值，高维度值无法取
+// 例如 int arr[2][3] = {{1,2,3},{4,5,6}}在定义类可以正常定义，但是使用时只能使用 arr[0][1] arr[1][1]这样的最底层维度，但是使用arr[0] arr[1]这样的高
+// 维度就无法编译通过
+//4. 在类内成员变量只有静态的时候才可以申明成constexpr否则编译失败
+//5. 要在类内初始化非整型的静态成员变量，必须是constexpr
+
+//6.static const constexpr等类内初始化的影响
+//    1.非静态成员变量，不管是什么类型都可以在类内初始化
+//    2.静态成员变量则必须是const属性，且类型必须是整型，如果不想是整型，则要用constexpr修饰，此时可以把类型放宽到字面值类型
+
+// 注意：以上讨论仅限于c++11,可能更高的标准放宽条件
+class ConstexprStaticMemberClass
+{
+    public:
+        static constexpr int d0 = 1;
+        static constexpr double d1 = 2;
+        //constexpr int d3 = 1; //d3不是静态成员变量，所以c++11下编译失败
+        static constexpr  char* arr0[4] = {"123", "456"}; //constexpr的静态成员变量必须在类内初始化,不能在类内申明类外定义
+        static constexpr  int arr1[2][3] = {{1, 2, 3}, {4, 5, 6}}; //constexpr的静态成员变量必须在类内初始化,不能在类内申明类外定义
+        //static constexpr LiteralConstClass1 d1 = {'1', 2, 3.3};//LiteralConstClass1有类内初始值，无法满足列表初始化条件，c++11下编译失败
+        static constexpr LiteralConstClass11 c0 = {'1', 2, 3.3};//LiteralConstClass11无类内初始化，满足列表初始化条件，编译成功
+        //static constexpr std::vector<int> vec0 = {1,2,3};//vector不是字面值类型，c++11下编译失败
+
+
+        //下面讨论static const constexpr对类型的要求
+        const std::vector<int> vec1 = {1,2,3};//vector不是字面值类型，但是也不是静态成员变量，编译成功
+        //static LiteralConstClass11 c1 = {'1', 2, 3.3};//vector是字面值类型，但是是静态成员变量，静态成员变量要想在类内初始化必须是整型类型，c++11下编译失败
+        //static int d4 = 1;//非cosnt的静态变量不能在类内初始化，不管变量是什么类型满足什么条件都不行，c++11下编译失败
+        int d8 = 1;
+        const int d7 = 1;//非静态成员变量，不管是什么类型都可以在类内初始化，但是如果是静态成员变量则必须是const属性，且类型必须是整型
+        static const int d5 = 1;//整型的const静态变量可以在类内初始化
+        //static const double d6 = 1.1;//非整型的const静态变量不可以在类内初始化，要想在类内初始化则把const改成constexpr，c++11下编译失败
+};
